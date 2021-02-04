984333e45a881b296b2b1eb05a315a0b2b380d3d
michael <mlmbufpi@gmail.com>
2021-02-04 11:44:36 -0300

    Add models, signals, slugs

--- django2/core/admin.py
+++ django2/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto
+#já deve ter feito o migrate
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome','preco','estoque','slug','criado','modificado','ativo')
--- django2/core/models.py
+++ django2/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+# SIGNALS
+#processamento antes e depois 
+from django.db.models import signals
+#slug 
+from django.template.defaultfilters import slugify
+
+class Base(models.Model):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de atualização', auto_now_add=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Produto(Base):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    estoque = models.IntegerField('Estoque')
+    imagem = StdImageField('Imagem', upload_to='produtos', variations={'thumb': (124,124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.nome
+
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome) 
+    #mike zero => mike-zero
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
+#antes de salva, assim q produto for enviado... executa a produto_pre_save
+
+
