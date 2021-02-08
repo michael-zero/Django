dce60da09d1f3722af79e5e278893e65bbbec0eb
michael <mlmbufpi@gmail.com>
2021-02-08 18:58:48 -0300

    controle de sessão na view Produto

--- django2/core/views.py
+++ django2/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .forms import ContatoForm, ProdutoModelForm
 from django.contrib import messages
+from .models import Produto
+from django.shortcuts import redirect
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'produtos' : Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
     form = ContatoForm(request.POST or None)
@@ -22,24 +27,23 @@ def contato(request):
     return render(request, 'contato.html', context)
 
 def produto(request):
-    if str(request.method) == 'POST':
-        form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            # prod = form.save(commit=False)
-            # print(f'Nome: {prod.nome}')
-            # print(f'Preço: {prod.preco}')
-            # print(f'Estoque: {prod.estoque}')
-            # print(f'Imagem: {prod.imagem}')
-
-            messages.success(request,'Produto salvo com sucesso!')
-            form = ProdutoModelForm()
+    # print(f'Usuário: {request.user}')
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request,'Produto salvo com sucesso!')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request,'Erro ao salvar o produto :/')
         else:
-            messages.error(request,'Erro ao salvar o produto :/')
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+    
+        return render(request, 'produto.html', context)
     else:
-        form = ProdutoModelForm()
-    context = {
-        'form': form
-    }
+        return redirect('index')
 
-    return render(request, 'produto.html', context)
