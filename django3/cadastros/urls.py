86937d97d5592a0aaf9332e5f77065551ed0d3d8
michael <mlmbufpi@gmail.com>
2021-02-19 14:38:04 -0300

    updateView

--- django3/cadastros/urls.py
+++ django3/cadastros/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path 
 from .views import CampoCreate, AtividadeCreate
+from .views import CampoUpdate, AtividadeUpdate
 
 urlpatterns = [
     path('cadastrar/campo/', CampoCreate.as_view(), name='cadastros-campo'),
-    path('cadastrar/atividade/',AtividadeCreate.as_view(), name='cadastrar-atividade')
+    path('cadastrar/atividade/',AtividadeCreate.as_view(), name='cadastrar-atividade'),
+    # EDITAR
+    path('editar/campo/<int:pk>/',CampoUpdate.as_view(), name='editar-campo'),
+    path('editar/atividade/<int:pk>/',AtividadeUpdate.as_view(), name='editar-atividade'),
 ]
--- django3/cadastros/views.py
+++ django3/cadastros/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Atividade, Campo
 from django.urls import reverse_lazy
 
@@ -13,5 +13,20 @@ class CampoCreate(CreateView):
 class AtividadeCreate(CreateView):
     model = Atividade
     fields = ['numero','descricao','pontos','detalhes', 'campo']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+
+# UPDATE
+
+class CampoUpdate(UpdateView):
+    model = Campo
+    fields = ['nome', 'descricao']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class AtividadeUpdate(UpdateView):
+    model = Atividade
+    fields = ['numero','descricao', 'pontos','detalhes', 'campo']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index')
